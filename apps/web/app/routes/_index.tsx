afd6bda8a241b984c4a69f22d14c21f466e9b23e
Mahmoud Abdelwahab <hey@mahmoud.codes>
2024-05-31 23:01:52 +0300

    Updates

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -12,7 +12,6 @@ import { ConnectionString } from "~/components/connection-string";
 import { DeployButton } from "~/components/deploy-button";
 import { SqlEditor } from "~/components/sql-editor";
 import { Message } from "~/components/message";
-import { Intro } from "~/components/intro";
 import { minDelay } from "~/lib/min-delay";
 import { regions } from "@instant-postgres/neon/regions";
 
@@ -68,7 +67,20 @@ export default function Index() {
 	return (
 		<div className="bg-black min-h-lvh px-6 pt-24 lg:px-8 md:px-4 w-full">
 			<div className="max-w-screen-lg mx-auto">
-				<Intro />
+				<div>
+					<h1 className="text-balance text-[32px] font-semibold leading-[0.9] tracking-extra-tight text-white xl:text-[56px] lg:text-[44px]">
+						Postgres in under a second
+					</h1>
+					<h2 className="text-balance mt-[18px] text-xl font-light tracking-extra-tight text-[#AFB1B6] xl:mt-4 xl:text-lg lg:mt-3 lg:text-base md:mt-2">
+						Instantly provison a Postgres database on
+						<a
+							className="text-white underline hover:no-underline underline-offset-4"
+							href="https://console.neon.tech/?ref=instantPostgres"
+						>
+							Neon
+						</a>
+					</h2>
+				</div>
 				<div className="my-16" />
 
 				<div className="flex flex-col items-start gap-10 w-full">
