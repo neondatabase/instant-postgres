0cae33cd37670e76629f52e9310ffff049d17fbb
Mahmoud Abdelwahab <hey@mahmoud.codes>
2024-06-06 12:41:39 +0300

    update

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -23,6 +23,8 @@ export const clientAction = async ({ request }: ClientActionFunctionArgs) => {
 
 	const API_URL = import.meta.env.VITE_API_URL;
 
+	console.log({ API_URL });
+
 	const client = hc<AppType>(API_URL, {
 		headers: {
 			"Content-Type": "application/json",
