a35a98e00fe9d5fe41159f805f8d7a02e7b7b7fb
Mahmoud Abdelwahab <hey@mahmoud.codes>
2024-06-07 22:15:27 +0300

    Update tooltip

--- apps/web/app/components/request-info.tsx
+++ apps/web/app/components/request-info.tsx
@@ -8,7 +8,6 @@ const RequestInfo = () => {
 	const hasCreatedProject = fetcher?.data?.result?.hasCreatedProject ?? false;
 
 	const timeToProvision = fetcher?.data?.result?.timeToProvision ?? 0;
-	const responseTime = fetcher?.data?.result?.responseTime;
 	const regionId = fetcher?.data?.result?.project
 		?.region_id as keyof typeof regions;
 	const region = regions[regionId]?.name;
@@ -19,41 +18,41 @@ const RequestInfo = () => {
 				<p className="animate-in fade-in slide-in-from-bottom font-mono text-xs leading-none tracking-extra-tight text-white tracking-extra-tight opacity-90">
 					Provisioned in {timeToProvision} ms, {region}{" "}
 				</p>
-				{responseTime && (
-					<Tooltip.Provider delayDuration={300}>
-						<Tooltip.Root>
-							<Tooltip.Trigger className="p-2 text-white rounded-[10px]">
-								<svg
-									className="w-4 h-4"
-									role="img"
-									aria-label="Query info"
-									xmlns="http://www.w3.org/2000/svg"
-									width="24"
-									height="24"
-									viewBox="0 0 24 24"
-									fill="none"
-									stroke="currentColor"
-									strokeWidth="2"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								>
-									<circle cx="12" cy="12" r="10" />
-									<path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
-									<path d="M12 17h.01" />
-								</svg>
-							</Tooltip.Trigger>
-							<Tooltip.Portal>
-								<Tooltip.Content
-									className="text-black data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[10px] bg-white px-[15px] py-[10px] text-xs leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
-									sideOffset={5}
-								>
-									The total round-trip duration is {responseTime} ms
-									<Tooltip.Arrow className="fill-white" />
-								</Tooltip.Content>
-							</Tooltip.Portal>
-						</Tooltip.Root>
-					</Tooltip.Provider>
-				)}
+
+				<Tooltip.Provider delayDuration={300}>
+					<Tooltip.Root>
+						<Tooltip.Trigger className="p-2 text-white rounded-[10px]">
+							<svg
+								className="w-4 h-4"
+								role="img"
+								aria-label="Query info"
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="currentColor"
+								strokeWidth="2"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							>
+								<circle cx="12" cy="12" r="10" />
+								<path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+								<path d="M12 17h.01" />
+							</svg>
+						</Tooltip.Trigger>
+						<Tooltip.Portal>
+							<Tooltip.Content
+								className="text-black data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[10px] bg-white px-[15px] py-[10px] text-xs leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
+								sideOffset={5}
+							>
+								Actual time taken to provision the database. It doesn't
+								represent the total round-trip time.
+								<Tooltip.Arrow className="fill-white" />
+							</Tooltip.Content>
+						</Tooltip.Portal>
+					</Tooltip.Root>
+				</Tooltip.Provider>
 			</div>
 		);
 	}
